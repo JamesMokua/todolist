6f0de0630c05dbfb707d251b7bb36b48ef66e802
JamesMokua <jamieedgar02@gmail.com>
2020-12-21 23:41:36 +0300

    todo

---
+++ Close/App.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import {View,Text,StyleSheet,FlatList,Alert} from 'react-native'
+import ListItem from './components/ListItem'
+import Header from './components/Header'
+import AddItem from './components/AddItem'
+
+const App = ()  => {
+  const[items,setItems] = useState([
+   
+  
+ 
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert("Error", "Please enter an item", [{text: 'Ok'}])
+    } else {
+       setItems(prevItems => {
+      return [{id: Math.random(), text},...prevItems]
+    });
+    }
+   
+  }
+  return(
+<View style={styles.container}>
+  <Header/>
+  <AddItem addItem={addItem}/>
+  <FlatList data={items} renderItem={({item}) => <ListItem item={item}  deleteItem={deleteItem}/>}
+  />
+</View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1 ,
+    paddingTop:60
+  }
+})
+
+export default App
